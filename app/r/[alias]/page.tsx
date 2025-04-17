e15162e953245de2673b65cf76755f19ebf5c314
Jonah Musher-Eizenman <jonahm@bu.edu>
2025-04-17 14:18:02 -0400

    Fixing errors

--- app/r/[alias]/page.tsx
+++ app/r/[alias]/page.tsx
@@ -2,8 +2,8 @@ import {redirect} from 'next/navigation';
 import getOneLink from '@/lib/getOneLink';
 export const dynamic = 'force-dynamic';
 
-export default async function GoToLink(props: {params:{alias: string}}){
-    const alias = props.params.alias;
+export default async function GoToLink(context: {params:{alias: string}}){
+    const alias = context.params.alias;
     const OGLink = await getOneLink(alias);
 
     if (!OGLink) {
