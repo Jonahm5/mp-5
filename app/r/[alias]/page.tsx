cd754844fe60c801aa53c360e83efaef6391d7d0
Jonah Musher-Eizenman <jonahm@bu.edu>
2025-04-17 14:00:35 -0400

    First Commit

--- app/r/[alias]/page.tsx
+++ app/r/[alias]/page.tsx
@@ -1,5 +1,6 @@
 import {redirect} from 'next/navigation';
 import getOneLink from '@/lib/getOneLink';
+export const dynamic = 'force-dynamic';
 
 export default async function GoToLink({params}: {params:{alias: string}}){
     const alias = params.alias;
